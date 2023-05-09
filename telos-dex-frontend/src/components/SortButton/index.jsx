81853130aed5c0ac2ba5c8e4c0f5d785aedde7f9
Pedro Fernandes <pedrohq_fernandes@hotmail.com>
2023-05-09 20:14:25 -0300

    Feat(SortButton):Creating the component 'SortButton'

---
+++ telos-dex-frontend/src/components/SortButton/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css'
+function SortButton() {
+  return (
+    <button className='SortButton'>
+        #
+    </button>
+  )
+}
+
+export default SortButton
