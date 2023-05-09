624e8c0aa52c5c35a1cbd1a84dabbd22e1f21128
Pedro Fernandes <pedrohq_fernandes@hotmail.com>
2023-05-09 20:15:15 -0300

    Feat(HomePage): Home page adjustments

--- telos-dex-frontend/src/pages/Home/index.jsx
+++ telos-dex-frontend/src/pages/Home/index.jsx
@@ -2,19 +2,26 @@ import React from 'react'
 import './index.css'
 import image from './Pokeball.png'
 import SearchInput from '../../components/SearchInput'
+import SortButton from '../../components/SortButton'
 
 
 function Home() {
   return (
+    <body>
     <header className='header'>
       <div className='headerTitle'>
       <img src={image} alt="" />
       <h1>Pokédex</h1>
       </div>
-      <div>
+      <div className='Search'>
         <SearchInput/>
+        <SortButton/>
       </div> 
     </header>
+    <main>
+      <h1>Teste</h1>
+    </main>
+    </body>
   )
 }
 
